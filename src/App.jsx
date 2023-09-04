b80632bc214975bb5bd19fd7418359cbcccacee4
Christopher <csbroox@gmail.com>
2023-09-04 14:47:37 -0500

    working on click

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   //console.log(puppies);
   //console.log('hello');
+  const [featPupId, setFeatPupId] = useState(null)
 
   return (
     <div className="App">
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+             return <p onClick={() => {}} key={puppy.id}>{puppy.name}</p>;
            })
        }
     </div>
