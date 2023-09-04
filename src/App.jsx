cb3b5c148975b47bac5b739380d83ef55672a350
Christopher <csbroox@gmail.com>
2023-09-04 14:41:29 -0500

    list on display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  //console.log(puppies);
+  //console.log('hello');
 
   return (
-    <>
-      <div>
-       
-      </div>
-      
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
